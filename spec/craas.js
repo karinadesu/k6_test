4de5eb85472d649de48060985eeda28f104c241f
yahina <yahina@selectel.ru>
2022-02-14 17:29:54 +0300

    Add time and speed

--- spec/craas.js
+++ spec/craas.js
@@ -7,7 +7,6 @@ import execution from 'k6/execution';
 const USERNAME = "token";
 const PASSWORD = "bf37e0e1-a9b2-4d6a-8266-1c6657c515f2";
 const BASEURL = "cr.selcloud.ru/";
-const d = new Date();
 
 export const options = {
   vus: 1,
@@ -17,17 +16,20 @@ export const options = {
 export default function testSuite() {
 
   let registryName = "yahina/";
-  let image = "3gbimag2e";
+  let image = "9gbimage";
   let tag = ":latest";
   let IMAGE = `${BASEURL}${registryName}${image}${tag}`;
   let res;
-  let text = d.toDateString();
-  let time = d.toTimeString();
+  
 
-  console.log("START TIME: ", text, time);
+  console.log(text)
 
   //we have to be sure that the image doesn't exist
   describe('01. Delete image', (t) => {
+
+    const d = new Date();
+    let text = d.toDateString();
+    console.log(text);
 
     res = exec.command("docker", ["system", "prune", "--volumes", "--all", "--force"]);
 
@@ -77,30 +79,5 @@ export default function testSuite() {
     let speed = image_size/pull_time;
     console.log(`\nIMAGE:\n${IMAGE}\n\nRESPONSE PULL:\n`, JSON.stringify(res), `\n\nPULL DURATION:\n${pull_time/60}min`, `\n\nSPEED:\n${speed}Mb/s`);
     sleep(1);
-    console.log("END TIME: ", text, time);
   });
-/*
-  //we have to delete the image for further downloads
-  describe('02. Delete image', (t) => {
-
-    let imagesList = (exec.command("docker", ["image", "ls"]));
-    let parseARR = imagesList.split('\n')
-
-    parseARR.forEach(line =>{
-
-      console.log("line ", line);
-
-      if (IMAGE.includes(line)) {
-
-          console.log(`SUCCESSFULLY FOUND IMAGE: ${IMAGE}`);
-          res = exec.command("docker", [ "rmi", "-f", `${IMAGE}${tag}`]);
-          console.log("Delete Image (last case): ", JSON.stringify(res));
-      }
-    })
-    check(res, {
-      'Delete image': (r) => r.includes(`Untagged: ${IMAGE}${tag}`)
-    });
-
-      sleep(1);
-  })*/
 }
